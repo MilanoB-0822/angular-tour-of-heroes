7654e4f930904acc1abb02f20d8e5337ef962cee
MilanoB-0822 <milanobr0822@gmail.com>
2022-09-07 23:20:17 +0200

    hero's list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import {Hero} from "./hero";
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Mr. Milano'},
+  { id: 2, name: 'Dr.Strange'},
+  { id: 3, name: 'the Equalizer'},
+  { id: 4, name: 'The climate Activist'},
+  { id: 5, name: 'Earth Woman'},
+  { id: 6, name: 'Forest Man'},
+  { id: 7, name: 'The Ocean Cleaner'},
+  { id: 8, name: 'Vertical farm boy'},
+  { id: 9, name: 'AquaPhonics Girl'},
+  { id: 10, name: 'The Volunteer'},
+
+]
